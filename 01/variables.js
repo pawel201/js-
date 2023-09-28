f0defed49dcd501cc60376ce04510f8b8f5a8b57
Pawel Kumar <89960996+pawel201@users.noreply.github.com>
2023-09-28 20:31:05 +0000

    done

---
+++ 01/variables.js
@@ -0,0 +1,11 @@
+const accountId =110
+let accountEmail = "pk@gmail.com"
+var accountPassword = " 1234"
+    accountCity=" Patna"
+    let accountState; //it gives only "undefined "
+ console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
+ /* 
+ Prefer to not use VAR
+ because of issue in block scope and functional scope
+ */  
