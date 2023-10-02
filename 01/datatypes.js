ac552a023c4b9a154084146daceff571ec2c6739
Pawel Kumar <89960996+pawel201@users.noreply.github.com>
2023-10-02 17:52:56 +0000

    save

---
+++ 01/datatypes.js
@@ -0,0 +1,19 @@
+let name="Pawel"
+let age= 19
+let isLoggedIn= true
+let state= null //enpty
+
+
+//number => 2 to power 53
+// bigint
+//string =>""
+//boolean => true/false
+//null => standalone value
+// undefined =>  value did'nt assigned
+// symbol => unique
+
+
+// object
+
+
+console.log(typeof age)
