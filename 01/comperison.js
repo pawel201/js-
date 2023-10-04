fb472c844a46b7a790f364aef3d1cb0eac6d7acd
Pawel Kumar <89960996+pawel201@users.noreply.github.com>
2023-10-04 08:35:43 +0000

    done

---
+++ 01/comperison.js
@@ -0,0 +1,20 @@
+// console.log(2>3)
+// console.log("2"<1)  //"2" converts in number
+// console.log(2>=1)
+// console.log(2==1)
+// console.log(2<1)
+
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+
+// Ther reason is that an equality check == and comperison < > >= <= work differently
+//comperison convert null to number , treating it as 0.
+
+// console.log(undefined ==0)
+// console.log(undefined >0)
+// console.log(undefined <0)
+
+console.log("2"===2)  // triple = also check t=data types
+
